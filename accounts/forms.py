cc8f1870a06cb0e10279ac2c9df9d31f0875c549
strongmove <94773539+strongmove@users.noreply.github.com>
2025-09-12 16:57:02 -0700

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(AdminUserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email")
